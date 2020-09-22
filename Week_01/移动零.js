762c16de0a8251644a36fdf4d82faee570dc889d
yxl5211 <1843933786@qq.com>
2020-09-22 16:45:41 +0800

    第一个编程题

---
+++ Week_01/移动零.js
@@ -0,0 +1,10 @@
+var nums = [0, 1, 0, 3, 12];
+var movezeroes = function(nums) {
+    for (let i = nums.length - 1; i >= 0; i--) {
+        let num = nums[i];
+        if (!num) {
+            nums.splice(i, 1);
+            nums.push(0);
+        }
+    }
+}
